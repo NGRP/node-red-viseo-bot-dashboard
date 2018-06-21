caae5ce432dda7c01fc868716b36e48e847bc571
aymericdeloche <delochedenoyel@et.esiea.fr>
2018-06-21 11:36:34 +0200

    Tachyon-elm implemented

--- client/my-app/src/Main.elm
+++ client/my-app/src/Main.elm
@@ -2,6 +2,8 @@ module Main exposing (..)
 
 import Html exposing (Html, text, div, h1, img)
 import Html.Attributes exposing (class, href, src, style)
+import Tachyons exposing (classes, tachyons)
+import Tachyons.Classes exposing (avenir, center, debug, debug_grid, flex, flex_column, mw5, mw6, vh_100)
 
 
 ---- MODEL ----
@@ -35,15 +37,32 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [ class "fl w-100 pv3 bg-green fl" ] [ text "HEADER" ]
-        , div [ class "" ]
-            [ div [ class "flex flex-column fl w-40" ]
-                [ div [ class "outline pv5 bg-blue fl w-100 " ] [ text "STATISTIQUES" ]
-                , div [ class "outline pv6 bg-pink fl w-100  " ] [ text "LISTE DES CONV" ]
+    -- div [ class "vh-100 w-100" ]
+    --     [ div [ class "fl w-100 bg-green fl" ] [ text "HEADER" ]
+    --     , div [ class "" ]
+    --         [ div [ class "flex flex-column fl w-40" ]
+    --             [ div [ class "outline bg-blue fl w-100 " ] [ text "STATISTIQUES" ]
+    --             , div [ class "outline bg-pink fl w-100  " ] [ text "LISTE DES CONV" ]
+    --             ]
+    --         ]
+    --     , div [ class "fl w-60 bg-yellow " ] [ text "CONVERSATIONS" ]
+    --     ]
+    div
+        []
+        --[ classes [ debug, debug_grid ]]
+        [ tachyons.css
+        , div
+            [ classes
+                [ flex
+                , flex_column
+                , avenir
+                , vh_100
+                , mw6
+                , center
                 ]
             ]
-        , div [ class "fl w-60 pv7  bg-yellow " ] [ text "CONVERSATIONS" ]
+            [ text "Hi mate"
+            ]
         ]
 
 
