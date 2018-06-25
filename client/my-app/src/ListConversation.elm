9be3cea7a2fdcbaf297c979198f05d5a3077d887
lenastm <lenastm@users.noreply.github.com>
2018-06-25 17:02:10 +0200

    liste en elm

--- client/my-app/src/ListConversation.elm
+++ client/my-app/src/ListConversation.elm
@@ -1,9 +1,31 @@
 module ListConversation exposing (init, Model, update, view, Msg)
 
-import Html exposing (Html, text, div, h1, img, a)
+import Html exposing (Html, text, div, h1, img, a, nav, ul, li)
 import Html.Attributes exposing (class, href, src, style)
 import Tachyons exposing (classes, tachyons)
-import Tachyons.Classes exposing (..)
+import Tachyons.Classes
+    exposing
+        ( outline
+        , bg_pink
+        , w_100
+        , list
+        , pl0
+        , ml0
+        , center
+        , mw6
+        , ba
+        , b__light_silver
+        , br2
+        , pa2
+        , pa4
+        , ph3
+        , pv3
+        , bb
+        , nowrap
+        , overflow_container
+        , f4
+        , no_underline
+        )
 
 
 ---- MODEL ----
@@ -40,13 +62,76 @@ view model =
     div
         [ classes
             [ outline
-            , bg_pink
             , w_100
             ]
         , class "liste"
         ]
-        [ text " LISTE DES CONV" ]
+        [ displayListExample
+        ]
 
 
+displayListExample : Html Msg
+displayListExample =
+    nav
+        [ classes
+            [ pa2
+            , pa4
+            ]
+        ]
+        [ h1
+            [ classes
+                [ f4
+                , center
+                ]
+            , class "center"
+            ]
+            [ text " Liste des conv" ]
+        , div
+            [ classes
+                [ nowrap
+                , overflow_container
+                ]
+            , class "listHeight"
+            ]
+            [ ul
+                [ classes
+                    [ list
+                    , pl0
+                    , center
+                    , mw6
+                    , ba
+                    , b__light_silver
+                    , br2
+                    , w_100
+                    ]
+                ]
+                [ displayLine
+                , displayLine
+                , displayLine
+                , displayLine
+                , displayLine
+                , displayLine
+                , displayLine
+                , displayLine
+                , displayLine
+                ]
+            ]
+        ]
 
----- PROGRAM ----
+
+displayLine : Html Msg
+displayLine =
+    li
+        [ classes
+            [ ph3
+            , pv3
+            , bb
+            ]
+        ]
+        [ a
+            [ classes
+                [ no_underline
+                ]
+            ]
+            [ text "lena" ]
+        ]
